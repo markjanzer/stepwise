d0756805ca5750a2fe92383a867ae87146c99773
markjanzer <mark.janzer@gmail.com>
2020-01-05 17:51:05 -0800

    Make tasks editable and deletable from index view

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import {
+  Button,
+  Checkbox,
+  Heading,
+  Link,
+  Pane,
+  Text,
+  TextInput
+} from "evergreen-ui";
+import { debounce } from "lodash";
+
 // import PropTypes from "prop-types"
 
 function Index(props) {
@@ -18,62 +29,120 @@ function Index(props) {
     );
   }
 
+  function deleteTask(taskToDelete) {
+    setTasks(tasks.filter(task => task.id !== taskToDelete.id));
+  }
+
+  function restoreTask(taskToRestore) {
+    // Not restoring in the correct order
+    if (tasks.some(task => task.id !== taskToRestore)) {
+      setTasks(tasks.concat(taskToRestore));
+    }
+  }
+
   return (
-    <section>
-      <h1>Two Weeks</h1>
-      {passions.map(passion =>
-        Passion({
-          passion,
-          passions,
-          updateTask,
-          tasks: tasks.filter(task => task.passion_id === passion.id)
-        })
-      )}
-    </section>
+    <Pane>
+      <Heading>Two Weeks</Heading>
+      {passions.map(passion => (
+        <Passion
+          {...{
+            passion,
+            passions,
+            updateTask,
+            deleteTask,
+            restoreTask,
+            key: passion.id,
+            tasks: tasks.filter(task => task.passion_id === passion.id)
+          }}
+        />
+      ))}
+    </Pane>
   );
 }
 
 function Passion(props) {
-  const { passion, tasks, passions, updateTask } = props;
+  const { passion, tasks, updateTask, deleteTask, restoreTask } = props;
 
   return (
-    <React.Fragment key={passion.id}>
-      <h2>
-        <a href={`/passions/${passion.id}`}>{passion.name}</a>
-      </h2>
-      <ul>
-        {tasks.map(task => Task({ task, passion, passions, updateTask }))}
-      </ul>
+    <React.Fragment>
+      <Link href={`/passions/${passion.id}`}>
+        <Heading>{passion.name}</Heading>
+      </Link>
+      <Pane>
+        {tasks.map(task => (
+          <Task
+            {...{ task, updateTask, deleteTask, restoreTask, key: task.id }}
+          />
+        ))}
+      </Pane>
     </React.Fragment>
   );
 }
 
 function Task(props) {
-  const { task, passion, passions } = props;
+  const { task } = props;
+  const [editable, setEditable] = useState(false);
 
-  function updateTask(data) {
+  // Update task state, then update db with optional debounce
+  // If server returns error, reset the task state
+  function updateTask(data, debounceMs = false) {
     const updatedTask = { ...task, ...data };
 
     props.updateTask(updatedTask);
 
-    axios.put(`/tasks/${task.id}`, data).catch(() => props.updateTask(task));
+    function updateDb() {
+      axios.put(`/tasks/${task.id}`, data).catch(() => props.updateTask(task));
+    }
+
+    if (debounceMs) {
+      debounce(updateDb, 400)();
+    } else {
+      updateDb();
+    }
   }
 
+  function deleteTask() {
+    axios.delete(`/tasks/${task.id}`).catch(() => props.restoreTask(task));
+    props.deleteTask(task);
+  }
+
+  if (editable) {
+    return (
+      <EditableTask
+        {...{ task, updateTask, deleteTask, setEditable, key: task.id }}
+      />
+    );
+  } else {
+    return <NonEditableTask {...{ task, setEditable, key: task.id }} />;
+  }
+}
+
+function EditableTask(props) {
+  const { task, updateTask, deleteTask, setEditable } = props;
+
   return (
-    <li key={task.id}>
-      <div>{task.description}</div>
-      <div>
-        {/* <Combobox 
-          openOnFocus
-          initialSelectedItem={passion}
-          items={passions}
-          itemToString={passion => passion.name ? passion.name : ""}
-          onChange={(passion) => updateTask({ passion_id: passion.id })}
-        /> */}
-      </div>
-      <div></div>
-      <div>{task.due_date}</div>
-    </li>
+    <Pane>
+      <TextInput
+        value={task.description}
+        onChange={e => updateTask({ description: e.target.value }, 600)}
+      />
+      <Text color="muted">Due {task.due_date}</Text>
+      <Button onClick={() => console.log("edit due date")}>Edit</Button>
+      <Button onClick={() => setEditable(false)}>Done</Button>
+      <Button onClick={() => deleteTask(task)}>Delete</Button>
+    </Pane>
+  );
+}
+
+function NonEditableTask(props) {
+  const { task, setEditable } = props;
+
+  return (
+    <Pane>
+      <Checkbox checked={task.completed_at} label={task.description}></Checkbox>
+      <Text color="muted">Due {task.due_date}</Text>
+      <Button onClick={() => setEditable(true)}>Edit</Button>
+    </Pane>
   );
 }
 
